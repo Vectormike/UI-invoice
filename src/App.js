96ef6d479f088dc0a9dddb88f523a9eeab0da395
vectormike40 <victorjonah199@gmail.com>
2019-08-05 13:24:43 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Home from "./components/Home/home";
+import Register from "./components/Registration/register";
+import Login from "./components/Login/login";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Route exact path="/" component={Home} />
+      <Route path="/register" component={Register} />
+      <Route path="/signin" component={Login} />
     </div>
   );
 }
